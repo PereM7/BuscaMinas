9c2af98b24ef94783ca8112c65e0f3f9a2678eba
PereMiquel <peremiquel5401@iessineu.net>
2026-03-04 21:02:41 +0100

    Afegit mètodes Tablero

--- Tablero.java
+++ Tablero.java
@@ -6,13 +6,6 @@ public class Tablero {
     private int llargari;
     private Casella[][] tauler;
     private int nombreMines = 1;
-    
-    private boolean verificarNombreMines (int nombreMines) {
-        if (nombreMines < (altura * llargari) ) {
-            return true;
-        }
-        return false;
-    }
 
     public Tablero (int altura, int llargari, int nombreMines) {
         this.altura = altura;
@@ -23,14 +16,34 @@ public class Tablero {
             this.nombreMines = nombreMines;
         } else {
             System.out.println("Nombre de mines major a caselles disponibles, default 1");
-        } 
+        }
+    }
+
+    public void iniciarTauler() {
+        rellenarArray();
+    }
+
+    private boolean verificarNombreMines (int nombreMines) {
+        if (nombreMines < (altura * llargari) ) {
+            return true;
+        }
+        return false;
+    }
+
+    private void rellenarArray () {
+        for (int fila = 0; fila < tauler.length; fila++) {
+            for (int columna = 0; columna < tauler[fila].length; columna++) {
+
+                tauler[fila][columna] = new Casella();
+            }
+        }
     }
 
     private void colocarMinesAleatoris () {
         int contadorMines = nombreMines;
         while (contadorMines > 0) {
-            int filaAleatori = (int) ((llargari + 1) * Math.random());
-            int columnaAleatori = (int) ((altura + 1) * Math.random());
+            int filaAleatori = (int) (llargari * Math.random());
+            int columnaAleatori = (int) (altura * Math.random());
 
             if (!tauler[filaAleatori][columnaAleatori].getEsMina()) {
                 tauler[filaAleatori][columnaAleatori].setEsMina(true);
@@ -41,5 +54,8 @@ public class Tablero {
 
     private void calcularMinesVoltantCasella () {
         
+        for (int contadorCas = 8; contadorCas > 0; contadorCas--) {
+            
+        }
     }
 }
